01ea1602eb59c5aee7bebb283c8b7859f9fb3e71
Tyson Caul <tjcaul@icloud.com>
2024-10-23 13:09:15 -0400

    Add Maven action for automatic building and testing (#1)
    * Add Maven action for automatic builds
    
    * Add pom.xml and Main.java for Maven
    
    * Change pom.xml path in maven.yml
    
    * Move pom.xml back
    
    * Remove dependency graph feature of Maven
    
    It was causing a 403 error
    
    * Add junit to maven
    
    + Add MainTest to check it works
    
    * Add Surefire to run tests
    
    * Add junit.jupiter to main dependencies
    
    * Change unit-platform-surefire-provider version
    
    * Change unit-platform-surefire-provider version
    
    * Intentionally break Main to fail test
    
    * Fix Main

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static int returnFive() {
+        return 5;
+    }
+}
