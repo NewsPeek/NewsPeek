943ead194e4e55165680a6df103fb925d307e992
Tyson Caul <tjcaul@icloud.com>
2024-11-07 03:04:20 -0500

    Add DataAccessObjects

---
+++ newspeek/src/main/java/data_access/APIDataAccessObject.java
@@ -0,0 +1,26 @@
+package data_access;
+
+import entity.Article;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// TODO: add `implements` statements for the DataAccessInterface for each use case
+/**
+ * DAO for to read articles from the web.
+ */
+public class APIDataAccessObject /* implements ______DataAccessInterface */ {
+    // TODO: implement
+    /*
+    public APIDataAccessObject(String csvPath, WebNewsArticleFactory articleFactory) throws IOException {
+
+    }
+
+    @Override
+    public Article get(String url) {
+        return WebNewsArticleFactory.create(...);
+    }
+    */
+}
---
+++ newspeek/src/main/java/data_access/FileArticleDataAccessObject.java
@@ -0,0 +1,27 @@
+package data_access;
+
+import entity.Article;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// TODO: add `implements` statements for the DataAccessInterface for each use case
+/**
+ * DAO for to read articles from files.
+ * Note that saving censored articles is done by ArticleSaveDataAccessObject, not this class.
+ */
+public class FileArticleDataAccessObject /* implements ______DataAccessInterface */ {
+    // TODO: implement
+    /*
+    public FileArticleDataAccessObject(String csvPath, FileArticleFactory articleFactory) throws IOException {
+
+    }
+
+    @Override
+    public Article get(String path) {
+        return FileArticleFactory.create(...);
+    }
+    */
+}
