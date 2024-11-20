19805ccb1dd371547c7d5c945bdf41d8fcf91456
Ray Chen <rc144@outlook.com>
2024-11-20 18:56:19 -0500

    add some more boilerplate

--- newspeek/src/main/java/use_case/choose_rule_set/ChooseRuleSetInputBoundary.java
+++ newspeek/src/main/java/use_case/choose_rule_set/ChooseRuleSetInputBoundary.java
@@ -1,4 +1,9 @@
 ﻿package use_case.choose_rule_set;
 
 public interface ChooseRuleSetInputBoundary {
+    /**
+     * Execute the Choose Rule Set use case.
+     * @param chooseRuleSetInputData the input data for the use case.
+     */
+    void execute(ChooseRuleSetInputData chooseRuleSetInputData);
 }
--- newspeek/src/main/java/use_case/choose_rule_set/ChooseRuleSetInteractor.java
+++ newspeek/src/main/java/use_case/choose_rule_set/ChooseRuleSetInteractor.java
@@ -1,4 +1,8 @@
 ﻿package use_case.choose_rule_set;
 
-public class ChooseRuleSetInteractor {
+public class ChooseRuleSetInteractor implements ChooseRuleSetInputBoundary{
+    @Override
+    public void execute(ChooseRuleSetInputData chooseRuleSetInputData) {
+
+    }
 }
