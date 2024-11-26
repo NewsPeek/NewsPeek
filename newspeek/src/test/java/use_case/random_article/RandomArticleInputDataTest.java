16a782ce013b2640e3170906c935c24fe7317624
Tyson Caul <tjcaul@icloud.com>
2024-11-25 22:49:30 -0500

    Add tests for Random Article use case (#49)
    * Add tests for Random Article use case
    
    * Add intentional failure mode to MemoryArticleDataAccessObject
    
    * Add documentation to Random Article use case and related methods
    
    * Suppress checkstyle in jreadability, since we didn't write it
    
    * Last relevant checkstyle error fixed!

--- newspeek/src/test/java/use_case/random_article/RandomArticleInputDataTest.java
+++ newspeek/src/test/java/use_case/random_article/RandomArticleInputDataTest.java
@@ -1,7 +1,14 @@
 package use_case.random_article;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import use_case.random_article.RandomArticleInputData;
 
 class RandomArticleInputDataTest {
-
+    @Test
+    void countryTest() {
+        RandomArticleInputData inputData = new RandomArticleInputData("Xyzzystan");
+        assertEquals("Xyzzystan", inputData.getCountry());
+    }
 }
--- newspeek/src/test/java/use_case/random_article/RandomArticleInteractorTest.java
+++ newspeek/src/test/java/use_case/random_article/RandomArticleInteractorTest.java
@@ -1,7 +1,81 @@
 package use_case.random_article;
 
+import data_access.article.MemoryArticleDataAccessObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class RandomArticleInteractorTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+class RandomArticleInteractorTest {
+    private RandomArticleAPIDataAccessInterface randomArticleDAO;
+
+    @BeforeEach
+    void createDAO() {
+        this.randomArticleDAO = new MemoryArticleDataAccessObject();
+    }
+
+    /**
+     * Test that when the DAO succeeds, RandomArticleInteractor will call
+     * RandomArticlePresenter.prepareSuccessView() with the correct article.
+     */
+    @Test
+    void successTest() {
+        RandomArticleInputData inputData = new RandomArticleInputData("us");
+
+        // Track whether presenter was called. A broken Interactor could just not call the presenter,
+        // and then it would pass the test as no assertions fail. This prevents that.
+        // Unfortunately, IntelliJ forces us to use a 1-element array instead of a single boolean.
+        // This is evil but there's probably a reason; something about being final.
+        final boolean[] presenterCalled = {false};
+
+        RandomArticleOutputBoundary successPresenter = new RandomArticleOutputBoundary() {
+            @Override
+            public void prepareSuccessView(RandomArticleOutputData outputData) {
+                presenterCalled[0] = true;
+                // From MemoryArticleDataAccessObject.makeMockArticle()
+                assertEquals("Sample Article", outputData.getArticle().getTitle());
+                assertFalse(outputData.isUseCaseFailed());
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                presenterCalled[0] = true;
+                fail("Use case should not fail with country 'us'.");
+            }
+        };
+
+        RandomArticleInteractor interactor = new RandomArticleInteractor(randomArticleDAO, successPresenter);
+        interactor.execute(inputData);
+        assertTrue(presenterCalled[0]); // make sure the Interactor actually called the presenter
+    }
+
+    /**
+     * Test that when the DAO throws an exception, RandomArticleInteractor will call
+     * RandomArticlePresenter.prepareFailView() with the exception's message.
+     */
+    @Test
+    void failureTest() {
+        RandomArticleInputData inputData = new RandomArticleInputData("FAIL");
+
+        // See comment in successTest() for explanation
+        final boolean[] presenterCalled = {false};
+
+        RandomArticleOutputBoundary successPresenter = new RandomArticleOutputBoundary() {
+            @Override
+            public void prepareSuccessView(RandomArticleOutputData outputData) {
+                presenterCalled[0] = true;
+                fail("Use case should not succeed with country 'FAIL'.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                presenterCalled[0] = true;
+                assertEquals("getRandomArticle intentionally failed.", error);
+            }
+        };
+
+        RandomArticleInteractor interactor = new RandomArticleInteractor(randomArticleDAO, successPresenter);
+        interactor.execute(inputData);
+        assertTrue(presenterCalled[0]); // make sure the Interactor actually called the presenter
+    }
 }
