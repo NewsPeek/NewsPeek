bc1b71abeee2fe2ec93449aae9d4f5ad92204035
Tyson Caul <tjcaul@icloud.com>
2024-12-02 13:53:53 -0500

    Add Article constructor without censorship statistics

--- newspeek/src/main/java/entity/article/Article.java
+++ newspeek/src/main/java/entity/article/Article.java
@@ -16,6 +16,38 @@ public class Article {
     private int censoredWords;
     private int replacedWords;
 
+    /**
+     * Constructor for uncensored article. Sets the censorship statistics to 0.
+     * @param title title of the article
+     * @param text full plaintext of the article
+     * @param source URL or filesystem source of the article
+     * @param author author of the article
+     * @param agency publishing agency of the article
+     * @param postedAt posting date and time of the article
+     */
+    public Article(String title, String text, String source, String author, String agency,
+                   java.time.LocalDateTime postedAt) {
+        this.title = title;
+        this.text = text;
+        this.author = author;
+        this.agency = agency;
+        this.source = source;
+        this.postedAt = postedAt;
+        this.censoredWords = 0;
+        this.replacedWords = 0;
+    }
+
+    /**
+     * Constructor for censored article. Specifies the censorship statistics.
+     * @param title title of the article
+     * @param text full plaintext of the article
+     * @param source URL or filesystem source of the article
+     * @param author author of the article
+     * @param agency publishing agency of the article
+     * @param postedAt posting date and time of the article
+     * @param censoredWords the number of words censored in the censorship process
+     * @param replacedWords the number of words replaced in the censorship process
+     */
     public Article(String title, String text, String source, String author, String agency,
                    java.time.LocalDateTime postedAt, int censoredWords, int replacedWords) {
         this.title = title;
@@ -95,12 +127,36 @@ public class Article {
         return postedAt;
     }
 
-    public int getCensoredWords() {return this.censoredWords;}
+    /**
+     * Returns the number of words censored (blocked out) in the censorship process.
+     * @return the number of words censored
+     */
+    public int getCensoredWords() {
+        return this.censoredWords;
+    }
 
-    public int getReplacedWords() {return this.replacedWords;}
+    /**
+     * Returns the number of words replaced in the censorship process.
+     * @return the number of words replaced
+     */
+    public int getReplacedWords() {
+        return this.replacedWords;
+    }
 
-    public void setCensoredWords(int censoredWord) {this.censoredWords=censoredWord;}
+    /**
+     * Sets the number of words censored (blocked out) in the censorship process.
+     * @param censoredWords the number of words censored
+     */
+    public void setCensoredWords(int censoredWords) {
+        this.censoredWords = censoredWords;
+    }
 
-    public void setReplacedWords(int replacedWord) {this.replacedWords=replacedWord;}
+    /**
+     * Sets the number of words replaced in the censorship process.
+     * @param replacedWords the number of words replaced
+     */
+    public void setReplacedWords(int replacedWords) {
+        this.replacedWords = replacedWords;
+    }
 }
 
