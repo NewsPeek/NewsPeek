1e2aa5df505d9651ab6838dad874fb4581560526
arnavchandna <arnavc08@gmail.com>
2024-12-03 07:19:13 -0500

    Tests progress

--- newspeek/src/test/java/use_case/ChooseRuleSet/ChooseRuleSetInputDataTest.java
+++ newspeek/src/test/java/use_case/ChooseRuleSet/ChooseRuleSetInputDataTest.java
@@ -1,5 +1,17 @@
 package use_case.ChooseRuleSet;
 
-public class ChooseRuleSetInputDataTest {
+import org.junit.jupiter.api.Test;
+import use_case.choose_rule_set.ChooseRuleSetInputData;
+import use_case.load_article.LoadArticleInputData;
 
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChooseRuleSetInputDataTest {
+    @Test
+    void inputTest() {
+        ChooseRuleSetInputData inputData = new ChooseRuleSetInputData(new File("xyz"));
+        assertEquals("xyz", inputData.getFile().getPath());
+    }
 }
--- newspeek/src/test/java/use_case/ChooseRuleSet/ChooseRuleSetOutputDataTest.java
+++ newspeek/src/test/java/use_case/ChooseRuleSet/ChooseRuleSetOutputDataTest.java
@@ -1,4 +1,16 @@
 package use_case.ChooseRuleSet;
 
+import entity.article.Article;
+import entity.censorship_rule_set.CensorshipRuleSet;
+import org.junit.jupiter.api.Test;
+import use_case.choose_rule_set.ChooseRuleSetOutputData;
+import use_case.load_article.LoadArticleOutputData;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ChooseRuleSetOutputDataTest {
+    @Test
+    void successTest() {
+
+    }
 }
